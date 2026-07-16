facdae159e4a15cbee01e0cec246693cc00b6c72
sherryahmos473 <sherry.ahmos@gmail.com>
2026-07-16 13:36:31 +0300

    refactor : move the form state locally within the View layer

--- app/src/main/java/com/iti/linguaquest/features/auth/presentation/login/contract/LoginState.kt
+++ app/src/main/java/com/iti/linguaquest/features/auth/presentation/login/contract/LoginState.kt
@@ -1,11 +1,7 @@
 package com.iti.linguaquest.features.auth.presentation.login.contract
 
-import com.iti.linguaquest.R
 
 data class LoginState(
-    val headerImageRes: Int = R.drawable.lingo,
-    val email: String = "",
-    val password: String = "",
     val isLoading: Boolean = false,
     val emailError: Boolean = false,
     val passwordError: Boolean = false,
--- app/src/main/java/com/iti/linguaquest/features/auth/presentation/signup/contract/SignUpState.kt
+++ app/src/main/java/com/iti/linguaquest/features/auth/presentation/signup/contract/SignUpState.kt
@@ -1,12 +1,6 @@
 package com.iti.linguaquest.features.auth.presentation.signup.contract
-import com.iti.linguaquest.R
 
 data class SignUpState(
-    val headerImageRes: Int = R.drawable.lingo_register,
-    val username: String = "",
-    val email: String = "",
-    val password: String = "",
-    val confirmPassword: String = "",
     val isLoading: Boolean = false,
     val usernameError: Boolean = false,
     val emailError: Boolean = false,
