fcb0541ae2da8c6414c1634beb41268f18b788cc
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2026-07-18 17:19:25 +0300

    remove duplicate di/modules

--- app/src/main/java/com/iti/linguaquest/features/home/di/HomeModule .kt
+++ app/src/main/java/com/iti/linguaquest/features/home/di/HomeModule .kt
@@ -3,19 +3,29 @@ package com.iti.linguaquest.features.home.di
 
 
 import com.iti.linguaquest.features.home.data.fake.FakeHomeRepository
+import com.iti.linguaquest.features.home.data.remote.HomeApiService
 import com.iti.linguaquest.features.home.domain.repository.HomeRepository
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-abstract class HomeRepositoryModule {
+abstract class HomeModule {
 
     @Binds
     @Singleton
     abstract fun bindHomeRepository(impl: FakeHomeRepository): HomeRepository
-    // Swap to: abstract fun bindHomeRepository(impl: HomeRepositoryImpl): HomeRepository when ready
+    // Swap to HomeRepositoryImpl when backend is ready
+
+    companion object {
+        @Provides
+        @Singleton
+        fun provideHomeApiService(retrofit: Retrofit): HomeApiService =
+            retrofit.create(HomeApiService::class.java)
+    }
 }
