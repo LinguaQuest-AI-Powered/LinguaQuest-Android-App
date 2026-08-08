d5300e6383cacbb5f22225cb9d1ad26bf7e3cd6f
Alaa7Hany <3laahanylol@gmail.com>
2026-08-08 23:02:08 +0300

    Refactor auth flow to use nativeLanguage object

--- app/src/main/java/com/iti/linguaquest/features/auth/domain/model/AuthUser.kt
+++ app/src/main/java/com/iti/linguaquest/features/auth/domain/model/AuthUser.kt
@@ -1,10 +1,11 @@
 package com.iti.linguaquest.features.auth.domain.model
 
+import com.iti.linguaquest.features.home.domain.model.LanguageOption
 data class AuthUser(
     val id: Int,
     val username: String?,
     val photo: String?,
-    val nativeLanguage: String?,
+    val nativeLanguage: LanguageOption?,
     val isVerified: Boolean,
     val targetLanguages: List<String>
 )
